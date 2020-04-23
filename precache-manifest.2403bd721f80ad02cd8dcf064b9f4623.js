741ada83538d780168e9621380ceb287236f46f3
francedot <francesco.bonacci@outlook.com>
2020-04-23 19:50:24 +0000

    Updates

---
+++ precache-manifest.2403bd721f80ad02cd8dcf064b9f4623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c04149644165fc4f568f44b7b6f9c7",
+    "url": "/carfront_example/index.html"
+  },
+  {
+    "revision": "f3f0d0da771f1d8df0c7",
+    "url": "/carfront_example/static/css/2.748935a2.chunk.css"
+  },
+  {
+    "revision": "dd3c918937d638135de0",
+    "url": "/carfront_example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3f0d0da771f1d8df0c7",
+    "url": "/carfront_example/static/js/2.b6cabf52.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/carfront_example/static/js/2.b6cabf52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3c918937d638135de0",
+    "url": "/carfront_example/static/js/main.ec4b8a29.chunk.js"
+  },
+  {
+    "revision": "197748b0af1a31e2c850",
+    "url": "/carfront_example/static/js/runtime-main.81cde9a8.js"
+  }
+]);
